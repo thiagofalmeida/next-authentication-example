3a287992a09e5414b1a65eaeae9fd3482c3a96aa
Thiago Ferreira de Almeida <thiagofalmd@gmail.com>
2021-05-10 15:59:03 -0300

    Validação e redirecionamento pelo lado do servidor

--- pages/dashboard.tsx
+++ pages/dashboard.tsx
@@ -1,17 +1,30 @@
 import { useContext, useEffect } from "react"
+import { setupAPIClient } from "../services/api"
+import { api } from "../services/apiClient"
+import { withSSRAuth } from "../utils/withSSRAuth"
 import { AuthContext } from "../contexts/AuthContext"
-import { api } from "../services/api"
 
 export default function Dashboard() {
   const { user } = useContext(AuthContext)
 
   useEffect(() => {
-    api.get('/me').then(response => {
-      console.log(response);
-    }).catch(err => console.error(err))
+    api.get('/me')
+      .then(response => console.log(response))
+      .catch(err => console.error(err))
   }, [])
 
   return (
     <h1>dashboard: {user?.email}</h1>
   )
 }
+
+export const getServerSideProps = withSSRAuth(async (ctx) => {
+  const apiClient = setupAPIClient(ctx)
+
+  const response = await apiClient.get('/me')
+  console.log(response.data);
+
+  return {
+    props: {}
+  }
+})
